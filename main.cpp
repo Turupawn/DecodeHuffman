bee7119034551c790b1836871b51407839b61ccd
Turupawn <ahmed.hn.43@gmail.com>
2017-02-22 16:42:17 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "Test.h"
+#include <iostream>
+using namespace std;
+
+string decodeHuffman(string code, BinaryNode* huffman_tree)
+{
+    return "";
+}
+
+int main ()
+{
+    test();
+    return 0;
+}
